d91d6d90d896e34a93684a31a57c64b9dec68cb2
Waris Ali <86531313+Devilboy04@users.noreply.github.com>
2021-08-08 19:28:16 +0530

    Update progress_for_pyrogram.py

--- tgtk/functions/progress_for_pyrogram.py
+++ tgtk/functions/progress_for_pyrogram.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K + YashDK [yash-dk@github] 
+# (c) Shrimadhav U K + Warisali [nothing-@github] 
 
 # the logging things
 import logging, asyncio
