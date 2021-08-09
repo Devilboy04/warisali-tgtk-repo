31fba1ca8324862a382f5129f5b977af37138ad5
Waris Ali <86531313+Devilboy04@users.noreply.github.com>
2021-08-09 09:02:11 +0530

    Update user_settings.py

--- tgtk/core/user_settings.py
+++ tgtk/core/user_settings.py
@@ -24,7 +24,7 @@ no = "✖"
 yes = "✔"
 # Central object is not used its Acknowledged 
 tordb = tkdb()
-header =  '<b>tgtk - a telegram leecher bot.\n</b>\n<u>user settings menu</u>'
+header =  '<b>ToolKit By WarisAli😈.\n</b>\n<u>user settings menu</u>'
 async def handle_user_setting_callback(e):
     db = tordb
     sender_id = str(e.sender_id)
